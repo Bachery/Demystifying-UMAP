9bef96370aefed8b38b92649b09ca36b40465811
Bachery <codeb.box@gmail.com>
2026-02-06 15:45:35 +0100

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	export const prerender = true;
+	export const ssr = false; // client-side rendering only, since this involves many WebGL and computations
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
