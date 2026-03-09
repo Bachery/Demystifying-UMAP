ab573176763dc7efdec941f11a7e197451d41a3c
Bachery <codeb.box@gmail.com>
2026-03-09 16:18:23 +0100

    vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 	import './layout.css';
 	// import favicon from '$lib/assets/favicon.svg';
 	import favicon from '/static/favicon/favicon.svg';
 
 	let { children } = $props();
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
